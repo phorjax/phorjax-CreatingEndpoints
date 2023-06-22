ceb52aa8b23b332c1e786ee76e5b24d648caea7c
phorjax <joseph.12082@gmail.com>
2023-06-22 23:37:42 +0000

    done

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -18,7 +18,10 @@ public class JavalinSingleton {
         Javalin app = Javalin.create();
         
         //write endpoint here
-
+        app.get("/hello", ctx -> {
+            //logic to be executed when this endpoint is hit
+            ctx.result("Hello World");
+        });
         return app;
     }
     
